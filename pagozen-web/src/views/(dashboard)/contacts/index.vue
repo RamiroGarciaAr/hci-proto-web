869812f689321007324697d68f2f88b049a67dfb
Tobias Juhasz <tjuhasz@itba.edu.ar>
2024-10-15 21:48:29 -0300

    Refactor project structure and add dashboard layout and components

---
+++ pagozen-web/src/views/(dashboard)/contacts/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <span>Contacts</span>
+</template>
---
+++ pagozen-web/tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "primary": {
+          "100": '#F2F6FE',
+          "200": '#B8CCF8',
+          "300": '#81A4F3',
+          "400": '#5F8CF0',
+          "500": '#3971EC',
+          "600": '#254897',
+          "700": '#142955',
+          "800": '#050A15',
+          "900": '#010102'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
